7a0f94b25c9b4fa4b752b7efd6e9dc587ecce288
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-08-25 20:04:04 -0400

    added workflow files

---
+++ .github/scripts/build_ros2.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# set up ROS
+distros=('foxy' 'galactic' 'humble')
+
+#
+# probe for the ROS2 distro
+#
+for distro in "${distros[@]}"
+do
+    if [[ -f "/opt/ros/${distro}/setup.bash" ]]; then
+	source /opt/ros/${distro}/setup.bash
+	break
+    fi
+done
+
+echo "found ros version: ${ROS_VERSION} distro: ${ROS_DISTRO}"
+# run wstool to bring in the additional repositories required
+wstool init src ./src/exposure_control_ros2/exposure_control_ros2.rosinstall
+
+# build and test
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo && colcon test && colcon test-result --verbose
+
+
